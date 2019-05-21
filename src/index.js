a71b1dc2f7ead53160897075827e7a5920420b20
Ivan Szebenszki <szib77@gmail.com>
2019-05-21 15:27:52 +0100

    Render dogbar and doginfo

--- src/index.js
+++ src/index.js
@@ -1,61 +1,47 @@
-const dogBar = document.querySelector('#dog-bar')
-const url = 'http://localhost:3000/pups'
-const dogInfo = document.querySelector('#dog-info')
+const URL = 'http://localhost:3000/pups';
+const dogBar = document.querySelector('#dog-bar');
+const dogInfo = document.querySelector('#dog-info');
 
-
-function dogSpan(dog) {
-    const dogEl = document.createElement('span')
-
-    dogEl.innerText = dog.name
-    dogEl.addEventListener('click', (event) => {
-        console.log(event)
-        addDogInfo(dog)
-    })
-    return dogEl
-}
-
-function fetchDogs(url) {
-    return fetch(url) 
-    .then(resp => resp.json())
-}
-
-function addDogsToBar(dogs) {
-    dogs.forEach(dog => {
-        const spanEl = dogSpan(dog)
-        dogBar.appendChild(spanEl)
-    })
-}
-
-function addDogInfo (dog) {
-    const buttonText = dog.isGoodDog ? 'Bad dog' : 'Good dog'
-    dogInfo.innerHTML = `
-    <img src=${dog.image}>
-    <h2> ${dog.name} </h2>
-    <button> ${buttonText} </button>
-    `
-    let dogButton = dogInfo.querySelector('button')
-    dogButton.addEventListener('click', (event) => {
-        fetch(`${url}/${dog.id}`, {
-            method: 'PATCH',
-            header: { 'Content-Type':'application/json'},
-            body: JSON.stringify({isGoodDog: !dog.isGoodDog})
-        }) 
-    })
-}
-
-function filterDogs(dogs, onlyGoodDogs = false) {
-    if (onlyGoodDogs) {
-      return dogs.filter(dog => dog.isGoodDog === true);
+const api = (url, options = {}) => fetch(url, options)
+  .then((resp) => {
+    if (resp.ok) {
+      return resp.json();
     }
-    return dogs;
+    return new Promise.reject(resp.json());
+  });
+
+const renderDogInfo = (dog) => {
+  dogInfo.innerHTML = '';
+
+  const titleEl = document.createElement('h2');
+  const imgEl = document.createElement('img');
+  const buttonEl = document.createElement('button');
+
+  titleEl.innerText = dog.name;
+  imgEl.src = dog.image;
+  buttonEl.innerText = dog.isGoodDog ? 'Bad dog' : 'Good dog';
+
+  dogInfo.append(titleEl, imgEl, buttonEl);
+};
+
+const renderDogSpan = (dog) => {
+  const spanEl = document.createElement('span');
+  spanEl.innerText = dog.name;
+
+  spanEl.addEventListener('click', () => renderDogInfo(dog));
+  return spanEl;
+};
+
+const renderDogbar = (dogs) => {
+  dogBar.innerHTML = '';
+  dogs.forEach((dog) => {
+    dogBar.appendChild(renderDogSpan(dog));
+  });
+};
+
+function init() {
+  api(URL)
+    .then(renderDogbar);
 }
 
-document
-
-function init () {
-    fetchDogs(url)
-    .then(dogs => addDogsToBar(dogs))
-    
-}
-
-init()
+init();
